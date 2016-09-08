c8391fa2783257eeecf01ab07f2758c22c3258a8
Igor-Tukh <igor-tukh@yandex.ru>
2016-09-08 20:37:22 +0300

    'Update'

--- Task 0/string_task.py
+++ Task 0/string_task.py
@@ -12,11 +12,11 @@ def not_bad(s):
 	badpos = s.find('bad')
 	notpos = s.find('not')
 	if (notpos < badpos and notpos != -1):
-		s = s[0:notpos:1] + 'good' + s[badpos+3:len(s):1]	
+		s = s[:notpos] + 'good' + s[badpos+3:]	
 	return s
 
 def front_back(a, b):
     la = len(a)
     lb = len(b)
 
-    return a[0:(la+1)//2:1] + b[0:(lb+1)//2:1] + a[(la+1)//2:la:1] + b[(lb+1)//2:lb:1]
+    return a[:(la+1)//2] + b[:(lb+1)//2] + a[(la+1)//2:] + b[(lb+1)//2:]
