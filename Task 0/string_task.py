0b15c77fceb651339f13c2c44d9c9669fd327011
Igor-Tukh <igor-tukh@yandex.ru>
2016-09-09 00:08:50 +0300

    Tabs

--- Task 0/string_task.py
+++ Task 0/string_task.py
@@ -1,19 +1,19 @@
 def verbing(s):
-	l = len(s)
-	if l >= 3:
-		if (s[-3:] == 'ing'):
-			s += 'ly'
-		else:
-			s += 'ing'
-	return s
+    l = len(s)
+    if l >= 3:
+        if (s[-3:] == 'ing'):
+            s += 'ly'
+        else:
+            s += 'ing'
+    return s
 
 
 def not_bad(s):
-	badpos = s.find('bad')
-	notpos = s.find('not')
-	if notpos < badpos and notpos != -1:
-		s = s[:notpos] + 'good' + s[badpos+3:]	
-	return s
+    badpos = s.find('bad')
+    notpos = s.find('not')
+    if notpos < badpos and notpos != -1:
+        s = s[:notpos] + 'good' + s[badpos+3:]    
+    return s
 
 def front_back(a, b):
     la = len(a)
--- Task 0/wordcount.py
+++ Task 0/wordcount.py
@@ -1,53 +1,53 @@
 import sys
  
 def print_words(filename):
-	total = get_words(filename)            # Using my own reading words from file that has been written earlier
-	#total = your_reading(filename) # This function use your reading words from file
-	total.sort()
+    #total = get_words(filename)            # Using my own reading words from file that has been written earlier
+    total = your_reading(filename) # This function use your reading words from file
+    total.sort()
 
-	for a, b in total:
-		print('{} {}'.format(a, b))
+    for a, b in total:
+        print('{} {}'.format(a, b))
 
-	#Output in text file - for more comfort
-	#fo = open('output.txt', 'w')
-	#for (a, b) in total:
-	#	fo.write(a + ' ' + str(b) + '\n')
+    #Output in text file - for more comfort
+    #fo = open('output.txt', 'w')
+    #for (a, b) in total:
+    #    fo.write(a + ' ' + str(b) + '\n')
 
 def print_top(filename):
-	total = get_words(filename) #using my own reading words from file that has been written earlier 
-	#total = your_reading(filename) # This function use your reading words from file
-	total.sort(key=lambda x: x[1])
+    #total = get_words(filename) #using my own reading words from file that has been written earlier 
+    total = your_reading(filename) # This function use your reading words from file
+    total.sort(key=lambda x: x[1])
  
-	for a, b in total[:-21:-1]:
-		print('{} {}'.format(a, b))
-   	
-	#Output in text file - for more comfort
-	#fo = open('output.txt', 'w')
-	#for (a, b) in total:
-	#	fo.write(a + ' ' + str(b) + '\n')
-	
+    for a, b in total[:-21:-1]:
+        print('{} {}'.format(a, b))
+       
+    #Output in text file - for more comfort
+    #fo = open('output.txt', 'w')
+    #for (a, b) in total:
+    #    fo.write(a + ' ' + str(b) + '\n')
+    
 def get_words(filename):            # My own reading words from file
-	f = open(filename, 'r')
-	words = []
-	for s in f.readlines():
-		s = s.lower()
-		cur1 = list(s.split())
-		for s1 in cur1:
-			s1.replace(',', '.')
-			s1.replace('!', '.')
-			s1.replace('?', '.')
-			s1.replace(':', '.')
-			cur2 = list(s1.split('.'))
-			for word in cur2:
-				if (len(word) > 0):
-					words.append(word)
-	dt = {}
-	for word in words:
-		if word in dt:
-			dt[word] = dt[word] + 1
-		else:
-			dt[word] = 1
-	return list(dt.items())
+    f = open(filename, 'r')
+    words = []
+    for s in f.readlines():
+        s = s.lower()
+        cur1 = list(s.split())
+        for s1 in cur1:
+            s1.replace(',', '.')
+            s1.replace('!', '.')
+            s1.replace('?', '.')
+            s1.replace(':', '.')
+            cur2 = list(s1.split('.'))
+            for word in cur2:
+                if (len(word) > 0):
+                    words.append(word)
+    dt = {}
+    for word in words:
+        if word in dt:
+            dt[word] = dt[word] + 1
+        else:
+            dt[word] = 1
+    return list(dt.items())
 
 def read_words(filename):
     words = []
@@ -55,17 +55,17 @@ def read_words(filename):
         for line in f:
             words.extend(line.split())
     return words
-	
+    
 def your_reading(filename):  # This function use your words reading
-	words = read_words(filename)
-	dt = {}
-	for word in words:
-		if word in dt:
-			dt[word] = dt[word] + 1
-		else:
-			dt[word] = 1
+    words = read_words(filename)
+    dt = {}
+    for word in words:
+        if word in dt:
+            dt[word] = dt[word] + 1
+        else:
+            dt[word] = 1
                       
-	return list(dt.items())
+    return list(dt.items())
 
 def main():
     if len(sys.argv) != 3:
